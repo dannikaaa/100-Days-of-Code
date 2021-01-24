99f6c19b483b12368a369fc05228f644cdcbaa09
Danica <danica_safina@yahoo.com>
2021-01-24 17:13:54 +1100

    Create Day2-Life-In-Weeks.py

---
+++ Beginner Section/Day2-Life-In-Weeks.py
@@ -0,0 +1,16 @@
+#Your life in weeks calculator
+
+#how many days,weeks,months do you have before turning 90?
+
+#there are 365 days, 52 weeks, 12 months in a year
+
+age = input("What is your current age?")
+int_age = int(age)
+
+years_left = 90 - int_age
+days_left = years_left * 365
+weeks_left = years_left * 52
+months_left = years_left * 12
+
+
+print(f"You have {days_left} days, {weeks_left} weeks, and {months_left} months left.")
