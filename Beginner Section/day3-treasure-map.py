a477ead6c326fd8a7b89d033cceaa9f32c1b4f35
Danica <danica_safina@yahoo.com>
2021-02-01 16:51:41 +1100

    Update day3-treasure-map.py

--- Beginner Section/day3-treasure-map.py
+++ Beginner Section/day3-treasure-map.py
@@ -6,8 +6,8 @@ print(f"{row1}\n{row2}\n{row3}")
 position = input("Where do you want to put the treasure? ")
 
 #the row & col beware
-col = int(position[0])
-row = int(position[1])
+col = int(position[0]) #first num
+row = int(position[1]) #second num 
 
 map[row - 1][col - 1] = "X"
 
